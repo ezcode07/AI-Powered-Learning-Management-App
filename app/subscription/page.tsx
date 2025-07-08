0ffc393d0d27a160dd2deae88ecdc842a464a00c
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-07-08 19:19:20 +0530

    Completed the Billing functionality of our app

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {PricingTable} from '@clerk/nextjs'
 
 const page = () => {
   return (
     <div>
-      
-        <p>Manage your subscription here.</p>
+      <h1 className='text-3xl font-bold text-center'>Subscription Plans</h1>
+       <PricingTable />
     </div>
   )
 }
