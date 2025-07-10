131e1e2c3210347e6ed9b5963e121d89837c1455
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-07-10 18:17:16 +0530

    Setuped superbase database

--- app/companion/new/page.tsx
+++ app/companion/new/page.tsx
@@ -1,7 +1,12 @@
 import CompanionForm from '@/components/CompanionForm'
 import React from 'react'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 
-const page = () => {
+const page = async () => {
+  const{userId} = await auth();
+  if(!userId) redirect('/sign-in');
+  
   return (
   <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
     <article>
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -3,8 +3,8 @@ import {PricingTable} from '@clerk/nextjs'
 
 const page = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold text-center'>Subscription Plans</h1>
+    <div className="m-10">
+      <h1 className='text-3xl font-bold text-center mb-5'>Subscription Plans</h1>
        <PricingTable />
     </div>
   )
